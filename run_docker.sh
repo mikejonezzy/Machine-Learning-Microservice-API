e72ccf59c564491848c3b2acc4890985891037b6
Mike Jones <jonezzy@gmail.com>
2022-06-23 18:08:36 +0100

    Corrected typo

--- run_docker.sh
+++ run_docker.sh
@@ -3,10 +3,10 @@
 ## Complete the following steps to get Docker running locally
 
 # Build image and add a descriptive tag
-docker build -t prediciton-app .
+docker build -t prediction-app .
 
 # List docker images
 docker images
 
 # Run flask app
-docker run -it --rm -p 8000:80 prediciton-app
+docker run -it --rm -p 8000:80 prediction-app
--- upload_docker.sh
+++ upload_docker.sh
@@ -3,13 +3,14 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=jonezzy/prediction-app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag prediction-app:latest $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
